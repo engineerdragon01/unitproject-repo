922ea0bd67ee53e100c23f34e9ec64b408cdd79f
Adan <adan1001917@gmail.com>
2019-07-27 23:27:04 -0700

    added handlers for the html pages

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import logging
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
 )
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {
+
+        }
+        template = jinja_env.get_template('templates/home.html')
+        self.response.write(template.render(template_vars))
+
 class LoginPage(webapp2.RequestHandler):
     def get(self):
         template_vars = {
@@ -15,7 +23,26 @@ class LoginPage(webapp2.RequestHandler):
         template = jinja_env.get_template('templates/login.html')
         self.response.write(template.render(template_vars))
 
+class TaskPage(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {
+
+        }
+        template = jinja_env.get_template('templates/task.html')
+        self.response.write(template.render(template_vars))
+        
+class QueuePage(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {
+
+        }
+        template = jinja_env.get_template('templates/queue.html')
+        self.response.write(template.render(template_vars))
+
 
 app = webapp2.WSGIApplication([
-    ('/', LoginPage)
+    ('/', MainPage),
+    ('/login', LoginPage),
+    ('/task', TaskPage),
+    ('/queue', QueuePage),
 ], debug = True)
