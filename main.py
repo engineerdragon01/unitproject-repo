ecfe6a6c1a23aa47d86076d473184c95d29253b7
Adan <adan1001917@gmail.com>
2019-07-27 23:10:40 -0700

    fixed errors with app yaml

---
+++ main.py
@@ -0,0 +1,21 @@
+
+import webapp2
+import jinja2
+import os
+import logging
+
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
+class UnitPage(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {
+
+        }
+        template = jinja_env.get_template('templates/login.html')
+        self.response.write(template.render(template_vars))
+
+
+app = webapp2.WSGIApplication([
+    ('/',UnitPage)
+], debug = True)
