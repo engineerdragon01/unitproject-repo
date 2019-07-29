e93829427f9c3b425f30b203bf9e1572b26d5aaa
Adan <adan1001917@gmail.com>
2019-07-29 11:05:37 -0700

    added login for datastore

--- main.py
+++ main.py
@@ -12,28 +12,73 @@ jinja_env = jinja2.Environment(
 )
 
 
-class User(ndb.Model):
-    user_name = ndb.StringProperty(required=True)
+class UnitUser(ndb.Model):
+    first_name = ndb.StringProperty()
+    last_name = ndb.StringProperty()
+    email = ndb.StringProperty()
 
 class Task(ndb.Model):
     task_name = ndb.StringProperty(required=True)
     description = ndb.StringProperty(required=True)
-    owner = ndb.KeyProperty(kind=User, required=True)
+    owner = ndb.KeyProperty(kind=UnitUser, required=True)
         # task_check
 
 # unit model class
 class Unit(ndb.Model):
     unit_name = ndb.StringProperty(required=True)
-    members = ndb.KeyProperty(kind=User, repeated=True)
+    members = ndb.KeyProperty(kind=UnitUser, repeated=True)
     task_keys = ndb.KeyProperty(kind=Task, repeated=True)
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        user_name = users.get_current_user()
+        user = users.get_current_user()
+        if user:
+            signout_link_html = '<a href="%s">sign out</a>' % (users.create_logout_url('/'))
+            email_address = user.nickname()
+            unit_user = UnitUser.query().filter(UnitUser.email == email_address).get()
+
+            if unit_user:
+
+                self.response.write('''
+                Welcome %s %s (%s)! <br> %s <br> ''' % (
+                unit_user.first_name,
+                unit_user.last_name,
+                email_address,
+                signout_link_html,
+                ))
+            else:
+                self.response.write('''
+                    Welcome to our site, %s!  Please sign up! <br>
+                    <form method="post" action="/">
+                    <input type="text" name="first_name" value="Enter First Name">
+                    <input type="text" name="last_name" value="Enter Last Name">
+                    <input type="submit">
+                    </form><br> %s <br>
+                    ''' % (email_address, signout_link_html))
+        else:
+
+            login_url = users.create_logout_url('/')
+            login_html_element = '<a href="%s">Sign in</a>' % login_url
+
+            self.response.write('Please log in.<br>' + login_html_element)
+
+    def post(self):
+
+        user = users.get_current_user()
+        unit_user = UnitUser(
+            first_name=self.request.get('first_name'),
+            last_name=self.request.get('last_name'),
+            email=user.nickname())
+        unit_user.put()
+        self.response.write('Thanks for signing up, %s! <br><a href="/">Home</a>' % unit_user.first_name)
         template_vars = {
-            'user_name': user_name
+
         }
+
+
+
+
         template = jinja_env.get_template('templates/home.html')
         self.response.write(template.render(template_vars))
 
@@ -54,15 +99,6 @@ class TaskPage(webapp2.RequestHandler):
         }
         template = jinja_env.get_template('templates/task.html')
         self.response.write(template.render(template_vars))
-
-class QueuePage(webapp2.RequestHandler):
-    def get(self):
-        template_vars = {
-
-        }
-        template = jinja_env.get_template('templates/queue.html')
-        self.response.write(template.render(template_vars))
-
 
 
 app = webapp2.WSGIApplication([
